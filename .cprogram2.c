18899a2ae6c091f1ed7f7a53f91fd46674511c16
kotteharshitha4-arch <kotteharshitha4@gmail.com>
2025-11-02 15:38:25 +0530

    radhakrishna
    introduction of c program

---
+++ .cprogram2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b,res;
+printf("enter the balance and withdrawal amount");
+scanf("%d",&a);
+scanf("%d",&b);
+res=a-b;
+printf("value of %d and %d is %d",a,b,res);
+return 0;
+}
